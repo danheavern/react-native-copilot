0c9e11b902c3d9f92d5ae5b4cc119d5222f32c2e
Dan Heavern <djheavern@aep.com>
2020-12-15 10:57:09 -0500

    Fix utilities - tests passing

--- src/utilities.js
+++ src/utilities.js
@@ -1,11 +1,11 @@
 // @flow
 import type { Step } from './types';
 
-export const getFirstStep = (steps: Array<Step>, group: string = steps[0].group): Step => Object
+export const getFirstStep = (steps: Array<Step>, group: string): Step => Object
   .values(steps)
   .reduce((a, b) => (!a || a.group !== group || a.order > b.order ? b : a), null);
 
-export const getLastStep = (steps: Array<Step>, group: string = steps[0].group): Step => Object
+export const getLastStep = (steps: Array<Step>, group: string): Step => Object
   .values(steps)
   .reduce((a, b) => (!a || a.group !== group || a.order < b.order ? b : a), null);
 
@@ -16,14 +16,14 @@ export const getStepNumber = (steps: Array<Step>, step: ?Step): number => step
 
 export const getPrevStep = (steps: Array<Step>,
   step: ?Step,
-  group: string = steps[0].group): number => Object
+  group: string): number => Object
   .values(steps)
   .filter(_step => _step.order < step.order && step.group === group)
   .reduce((a, b) => (!a || a.order < b.order ? b : a), null);
 
 export const getNextStep = (steps: Array<Step>,
   step: ?Step,
-  group: string = steps[0].group): number => Object
+  group: string): number => Object
   .values(steps)
   .filter(_step => _step.order > step.order && step.group === group)
   .reduce((a, b) => (!a || a.order > b.order ? b : a), null) || step;
